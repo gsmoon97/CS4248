e9a9151697c5eb7d1d2f1dbc404f7cc2ac817934
gsmoon97 <gsmoon97@gmail.com>
2021-11-13 23:31:33 +0800

    debug

--- Project/GECwBERT/gec_single.py
+++ Project/GECwBERT/gec_single.py
@@ -130,9 +130,13 @@ def check_GE(sents, modelGED):
     logits = logits.detach().cpu().numpy()
     # label_ids = b_labels.to("cpu").numpy()
 
+    print('logtis:\n{}\n'.format(logits))
+
     # Store predictions and true labels
     predictions.append(logits)
     # true_labels.append(label_ids)
+
+    print('predictions:\n{}\n'.format(predictions))
 
     temp = [item for sublist in predictions for item in sublist]
     prob_vals = temp
